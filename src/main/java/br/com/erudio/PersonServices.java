0d3964f6b8d5da9e4ef657fb048279b2dc9714ce
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-05 18:11:05 -0300

    Verbos HTTP.

--- src/main/java/br/com/erudio/PersonServices.java
+++ src/main/java/br/com/erudio/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,17 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
 
     public Person findById(String id) {
         logger.info("Finding one Person!");
@@ -24,5 +37,34 @@ public class PersonServices {
         person.setGender("Male");
 
         return person;
+    }
+
+    public Person mockPerson(int i) {
+        logger.info("Finding all People!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName" + i);
+        person.setLastName("LastName" + i);
+        person.setAddress("Some address in Brasil");
+        person.setGender("Male");
+
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one Person!");
     }
 }
--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,10 +4,9 @@ import br.com.erudio.PersonServices;
 import br.com.erudio.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/person")
 @RestController
@@ -23,5 +22,37 @@ public class PersonController {
     public Person findById(@PathVariable("id") String id) {
         return service.findById(id);
     }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        service.delete(id);
+    }
+
 
 }
