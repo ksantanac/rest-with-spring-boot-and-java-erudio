2a6e638d1eef3c69b35cc62e7220e0ef80771b88
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-07 12:55:05 -0300

    Teste.

--- src/main/java/br/com/erudio/mapper/ObjectMapper.java
+++ src/main/java/br/com/erudio/mapper/ObjectMapper.java
@@ -19,7 +19,7 @@ public class ObjectMapper {
 
         List<D> destinationObjects = new ArrayList<>();
         for (Object o: origin) {
-            destinationObjects.add(mapper.map(origin, destination));
+            destinationObjects.add(mapper.map(o, destination));
         }
 
         return destinationObjects;
