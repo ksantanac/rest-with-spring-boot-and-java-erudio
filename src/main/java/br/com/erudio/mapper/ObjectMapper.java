09785f7a95d94a43e68463b4e06d95e332b219b8
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-10 17:47:24 -0300

    Modificando retorno do Json.

--- src/main/java/br/com/erudio/mapper/ObjectMapper.java
+++ src/main/java/br/com/erudio/mapper/ObjectMapper.java
@@ -8,17 +8,22 @@ import java.util.List;
 
 public class ObjectMapper {
 
-    //  Mapear objeto de origem para objeto de destino
+    // Criando uma instância do Mapper do Dozer para mapear objetos entre diferentes classes
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
+    // Converte um objeto de um tipo para outro.
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
     }
 
+    // Converte uma lista de objetos de um tipo para outro.
     public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
 
+        // Criando uma nova lista para armazenar os objetos convertidos
         List<D> destinationObjects = new ArrayList<>();
-        for (Object o: origin) {
+
+        // Iterando sobre a lista de origem e convertendo cada objeto
+        for (O o : origin) {
             destinationObjects.add(mapper.map(o, destination));
         }
 
