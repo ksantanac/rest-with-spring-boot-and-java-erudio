2f3577187d9bb6acccbdf3a1220f253f199d09b8
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-10 18:29:37 -0300

    Custom Json Serialization.

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RequestMapping("/api/person/v1")
@@ -19,7 +20,17 @@ public class PersonController {
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonDTO findById(@PathVariable("id") Long id) {
-        return service.findById(id);
+
+        var person = service.findById(id);
+
+        person.setBirthDay(new Date());
+//        person.setPhoneNumber("+ 55 (11) 99283-3699");
+        person.setPhoneNumber("");
+        person.setLastName(null);
+        person.setSensitiveData("Foo Bar");
+
+        return person;
+//        return service.findById(id);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
