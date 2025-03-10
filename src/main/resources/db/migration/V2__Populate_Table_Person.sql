deb1f017f1e69ef38e2d7b5e54548c20807f9731
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-10 15:44:34 -0300

    Migrations.

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+-- Copiando dados para a tabela rest_with_spring_boot_erudio.person: ~8 rows (aproximadamente)
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil - 2015', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Santana de Parnaiba - SP - Brasil', 'Kaue', 'Male', 'Santana'),
+	(4, 'Favela - India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Favela - India', 'Mahama', 'Male', 'Gandhi'),
+	(7, 'Campo Grande - SP - Brasil', 'Karina', 'Female', 'Anjos'),
+	(9, 'Alphaville - SP - Brasil', 'Fabio', 'Male', 'Ippolito'),
+	(10, 'Alphaville - SP - Brasil', 'Fabio', 'Male', 'Ippolito'),
+	(11, 'Barueri - SP - Brasil', 'Lorenzo', 'Male', 'Ippolito');
