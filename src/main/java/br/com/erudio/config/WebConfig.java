0e7054222c85e1864f4ccf7d4f6f8a36f0538733
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-11 19:10:37 -0300

    Serializando em xml e json.

--- src/main/java/br/com/erudio/config/WebConfig.java
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -13,7 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
         // Via EXTENSION. http://localhost:8080/api/person/v1/4.xml http://localhost:8080/api/person/v1/4.JSON Deprecated on Spring Boot 2.6
+
         // Via QUERY PARAM http://localhost:8080/api/person/v1/4?mediaType=xml
+        /**
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
@@ -21,7 +23,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML);
+        */
 
-
+        // Via HEADER PARAM http://localhost:8080/api/person/v1/4
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
     }
 }
+
