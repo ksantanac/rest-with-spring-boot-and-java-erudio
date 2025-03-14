95ce94ef4e795822dc0f6a2c3fd5c98c6bf460c8
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-14 19:14:35 -0300

    Criando um Teste de Integração para Validar a Aplicação de CORS.

---
+++ src/test/java/br/com/erudio/integrationtests/controllers/withjson/PersonControllerTest.java
@@ -0,0 +1,105 @@
+package br.com.erudio.integrationtests.controllers.withjson;
+
+import br.com.erudio.config.TestConfigs;
+import br.com.erudio.integrationtests.dto.PersonDTO;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // JUnit roda os teste aleatoriamente, caso dependa do teste anterior nao dará certo. Com isso, usa o order
+class PersonControllerTest extends AbstractIntegrationTest {
+
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+
+    private static PersonDTO person;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        person = new PersonDTO();
+    }
+
+    @Test
+    @Order(1)
+    void create() throws JsonProcessingException {
+        mockPerson();
+
+        specification = new RequestSpecBuilder()
+            .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, TestConfigs.ORIGIN_ERUDIO)
+            .setBasePath("/api/person/v1")
+            .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+            .build();
+
+        var content = given(specification)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(person)
+            .when()
+                .post()
+            .then()
+                .statusCode(200)
+            .extract()
+                .body()
+                    .asString();
+
+        PersonDTO createdPerson = objectMapper.readValue(content, PersonDTO.class);
+        person = createdPerson;
+
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Richard", createdPerson.getFirstName());
+        assertEquals("Stallman", createdPerson.getLastName());
+        assertEquals("New York City - New York - EUA", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+
+    }
+
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    private void mockPerson() {
+        person.setFirstName("Richard");
+        person.setLastName("Stallman");
+        person.setAddress("New York City - New York - EUA");
+        person.setGender("Male");
+    }
+}
