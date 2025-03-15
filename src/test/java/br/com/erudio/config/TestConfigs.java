542ac0daefe18a15e3ccfebcab27ee16cd585181
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-15 15:47:01 -0300

    Testando a Aplicação de CORS com Diferentes Origins.

--- src/test/java/br/com/erudio/config/TestConfigs.java
+++ src/test/java/br/com/erudio/config/TestConfigs.java
@@ -8,4 +8,6 @@ public interface TestConfigs {
 
     String ORIGIN_ERUDIO = "https://www.erudio.com.br";
     String ORIGIN_SEMERU = "https://www.semeru.com.br";
+
+    String ORIGIN_LOCAL = "http://localhost:8080";
 }
--- src/test/java/br/com/erudio/integrationtests/controllers/withjson/PersonControllerTest.java
+++ src/test/java/br/com/erudio/integrationtests/controllers/withjson/PersonControllerTest.java
@@ -29,8 +29,8 @@ class PersonControllerTest extends AbstractIntegrationTest {
 
     private static PersonDTO person;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
@@ -79,21 +79,99 @@ class PersonControllerTest extends AbstractIntegrationTest {
 
     }
 
+    @Test
+    @Order(2)
+    void createWithWrongOrigin() throws JsonProcessingException {
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_SEMERU)
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        var content = given(specification)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(person)
+                .when()
+                .post()
+                .then()
+                .statusCode(403)
+                .extract()
+                .body()
+                .asString();
+
+        assertEquals("Invalid CORS request", content);
+
+    }
+
 
     @Test
-    void findById() {
+    @Order(3)
+    void findById() throws JsonProcessingException {
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, TestConfigs.ORIGIN_LOCAL)
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        var content = given(specification)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .pathParam("id", person.getId())
+                .when()
+                .get("{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        PersonDTO createdPerson = objectMapper.readValue(content, PersonDTO.class);
+        person = createdPerson;
+
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Richard", createdPerson.getFirstName());
+        assertEquals("Stallman", createdPerson.getLastName());
+        assertEquals("New York City - New York - EUA", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+
     }
 
     @Test
-    void update() {
-    }
+    @Order(4)
+    void findByIdWithWrongOrigin() throws JsonProcessingException {
 
-    @Test
-    void delete() {
-    }
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_SEMERU)
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
 
-    @Test
-    void findAll() {
+        var content = given(specification)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .pathParam("id", person.getId())
+                .when()
+                .get("{id}")
+                .then()
+                .statusCode(403)
+                .extract()
+                .body()
+                .asString();
+
+        assertEquals("Invalid CORS request", content);
     }
 
     private void mockPerson() {
