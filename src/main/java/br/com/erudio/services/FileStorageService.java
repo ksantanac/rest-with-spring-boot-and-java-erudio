01292bc2d25b2ccb35812e0d4673b443e728f784
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-18 23:08:50 -0300

    Preparando o Serviço para Prover Downloads.

--- src/main/java/br/com/erudio/services/FileStorageService.java
+++ src/main/java/br/com/erudio/services/FileStorageService.java
@@ -1,10 +1,13 @@
 package br.com.erudio.services;
 
 import br.com.erudio.config.FileStorageConfig;
+import br.com.erudio.exception.FileNotFoundException;
 import br.com.erudio.exception.FileStorageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -57,6 +60,24 @@ public class FileStorageService {
         } catch (Exception e) {
             logger.error("Could not store file " + fileName + ". Please try Again!");
             throw new FileStorageException("Could not store file " + fileName + ". Please try Again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists()) {
+                return resource;
+            } else {
+                logger.error("File not found " + fileName);
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+
+        } catch (Exception e) {
+            logger.error("File not found " + fileName);
+            throw new FileNotFoundException("File not found " + fileName, e);
         }
     }
 
