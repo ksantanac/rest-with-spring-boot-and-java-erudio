153f49d2492133703d080f48a3f536ecb675936b
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-03-26 18:44:37 -0300

    Criando Exceptions Customizadas e o DTO de Credenciais

---
+++ src/main/java/br/com/erudio/data/dto/security/TokenDTO.java
@@ -0,0 +1,87 @@
+package br.com.erudio.data.dto.security;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class TokenDTO implements Serializable {
+
+    private String username;
+    private Boolean authenticated;
+    private Date created;
+    private Date expiration;
+    private String acessToken;
+    private String refreshToken;
+
+    public TokenDTO() {}
+
+    public TokenDTO(String username, Boolean authenticated, Date created, Date expiration, String acessToken, String refreshToken) {
+        this.username = username;
+        this.authenticated = authenticated;
+        this.created = created;
+        this.expiration = expiration;
+        this.acessToken = acessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Boolean getAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAuthenticated(Boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getAcessToken() {
+        return acessToken;
+    }
+
+    public void setAcessToken(String acessToken) {
+        this.acessToken = acessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        TokenDTO tokenDTO = (TokenDTO) o;
+        return Objects.equals(username, tokenDTO.username) && Objects.equals(authenticated, tokenDTO.authenticated) && Objects.equals(created, tokenDTO.created) && Objects.equals(expiration, tokenDTO.expiration) && Objects.equals(acessToken, tokenDTO.acessToken) && Objects.equals(refreshToken, tokenDTO.refreshToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, authenticated, created, expiration, acessToken, refreshToken);
+    }
+
+}
